102a9f9b66ddb151ad8ccfe0902abf13fd27a484
Greg Woodbury <gwoodbury@highvistastrategies.com>
2017-10-03 15:07:20 -0400

    breaking out view templates into different dirs

--- glycemiq_web/account/views.py
+++ glycemiq_web/account/views.py
@@ -5,3 +5,13 @@ from . import account
 @account.route('/')
 def index():
     return render_template('index.html')
+
+
+@account.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@account.route('/login')
+def login():
+    return render_template('login.html')
