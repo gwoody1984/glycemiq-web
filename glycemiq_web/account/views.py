5e6fd8c4bf0787434bbb81295c43d50301a7609a
Greg Woodbury <gwoodbury@highvistastrategies.com>
2017-10-04 14:27:06 -0400

    email confirm on registration

--- glycemiq_web/account/views.py
+++ glycemiq_web/account/views.py
@@ -1,21 +1,80 @@
-from flask import render_template
+import bcrypt
+
+from flask import render_template, url_for, abort, redirect
+from itsdangerous import URLSafeTimedSerializer
 
 from . import account
 from .register_form import RegisterForm
+from ..emailer import send_mail
+from ..config import config_as_dict
+from glycemiq_db import db, User
+
+config = config_as_dict('APP')
+ts = URLSafeTimedSerializer(config['URL_SERIALIZER_KEY'])
+
 
 @account.route('/')
 def index():
     return render_template('index.html')
 
 
-@account.route('/register', methods=['GET','POST'])
+@account.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
-        return render_template('index.html')
+
+        # check for user existence
+        if User.query.filter_by(email=form.email.data).scalar() is None:
+            # new user save record
+            salt = bcrypt.gensalt()
+            user = User(
+                email=form.email.data,
+                password=salt + bcrypt.hashpw(form.password.data.encode('utf-8'), salt=salt)
+            )
+            db.session.add(user)
+            db.session.commit()
+
+            # send confirm email
+            token = ts.dumps(user.email, salt=config['URL_SERIALIZER_SALT'])
+
+            confirm_url = url_for(
+                'account.confirm_email',
+                token=token,
+                _external=True)
+
+            html = render_template(
+                'email/activate.html',
+                confirm_url=confirm_url)
+
+            send_mail(user.email, "Confirm your email", html)
+
+        else:
+            # user exists
+            # send user exists email
+            html = render_template('email/reregistration.html')
+            send_mail(form.email.data, "Your account already exists", html)
+
+        return render_template('registration_success.html')
     return render_template('register.html', form=form)
 
 
 @account.route('/login')
 def login():
     return render_template('login.html')
+
+
+@account.route('/confirm/<token>')
+def confirm_email(token):
+    try:
+        email = ts.loads(token, salt=config['URL_SERIALIZER_SALT'], max_age=86400)
+    except:
+        return abort(404)
+
+    user = User.query.filter_by(email=email).first_or_404()
+
+    user.verified = True
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect(url_for('login'))
---
+++ glycemiq_web/emailer.py
@@ -0,0 +1,27 @@
+import smtplib
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from .config import config_as_dict
+
+
+def send_mail(to_address, message_subject, message_body):
+    config = config_as_dict('APP')
+
+    user = config['MAIL_USER']
+    password = config['MAIL_PASSWORD']
+
+    msg = MIMEMultipart()
+    msg['From'] = user
+    msg['To'] = to_address
+    msg['Subject'] = message_subject
+    msg.attach(MIMEText(message_body))
+
+    mail_server = smtplib.SMTP(config['MAIL_SERVER'], config['MAIL_PORT'])
+    mail_server.ehlo()
+    mail_server.starttls()
+    mail_server.ehlo()
+    mail_server.login(user, password)
+    mail_server.sendmail(user, to_address, msg.as_string())
+    mail_server.close()
